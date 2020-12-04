03c8f18e881f9e99d9fdb7d76323d07328a1c6d9
Dhika Rizky <deka_r@outlook.com>
2020-12-05 03:17:24 +0700

    Added image upload capability

--- src/app/pages/new-report/new-report.page.ts
+++ src/app/pages/new-report/new-report.page.ts
@@ -3,8 +3,10 @@ import { ImagePicker } from '@ionic-native/image-picker/ngx'
 import { Platform, ToastController } from '@ionic/angular'
 import { Router } from '@angular/router'
 import { FirebaseX } from '@ionic-native/firebase-x/ngx'
+import { AngularFireStorage } from '@angular/fire/storage'
 
 import regions, { Cities, Districts, Provinces } from '../../helper/region'
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-new-report',
@@ -22,6 +24,7 @@ export class NewReportPage implements OnInit {
   selectedDistrict: string
   address: string
   imageUrl: string
+  defaultImage: string
 
   provinces: Provinces[]
   cities: Cities[]
@@ -32,12 +35,14 @@ export class NewReportPage implements OnInit {
       public platform: Platform,
       public toastController: ToastController,
       private firebase: FirebaseX,
-      private router: Router
+      private router: Router,
+      private storage: AngularFireStorage
     ) {
     this.provinces = regions.getProvinces()
     this.cities = []
     this.districts = []
     this.imageUrl = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAAAAACH5BAAAAAAALAAAAAABAAEAAAICTAEAOw=='
+    this.defaultImage = this.imageUrl
     firebase.getToken().then(token => console.log(token))
   }
 
@@ -61,7 +66,7 @@ export class NewReportPage implements OnInit {
   selectImage() {
     this.imagePicker.getPictures({ allow_video: false, maximumImagesCount: 1, outputType: 1, width: 720, height: 720, quality: 65}).then((results) => {
       if (results.length > 0) {
-        this.imageUrl = `data:image/gif;base64,${results[0]}`
+        this.imageUrl = `data:image/png;base64,${results[0]}`
       }
     }, (err) => { });
   }
@@ -71,38 +76,56 @@ export class NewReportPage implements OnInit {
   }
 
   async handleSubmit() {
-    const data = {
-      name: this.name,
-      age: this.age,
-      gender: this.gender,
-      province: this.selectedProvince,
-      city: this.selectedCity,
-      district: this.selectedDistrict,
-      address: this.address
-    }
-
-    await this.firebase.addDocumentToFirestoreCollection({
-      ...data,
-      deleted: false
-    },
-    'reportedCases',
-    async (id) => { 
-      console.log(id)
-      const toast = await this.toastController.create({
-        message: "Report Submitted!",
-        duration: 2500
+    try {
+      const data = {
+        name: this.name,
+        age: this.age,
+        gender: this.gender,
+        province: this.selectedProvince,
+        city: this.selectedCity,
+        district: this.selectedDistrict,
+        address: this.address
+      }
+  
+      let filePath: Observable<any> = null
+      
+      if (this.imageUrl !== this.defaultImage) {
+        const imgRef = this.storage.ref(`images/${this.name}_${new Date().getTime}_${this.selectedDistrict}.png`)
+        await imgRef.putString(this.imageUrl, 'data_url')
+        filePath = imgRef.getDownloadURL()
+      }
+  
+      await this.firebase.addDocumentToFirestoreCollection({
+        ...data,
+        picture: filePath ?? null,
+        deleted: false
+      },
+      'reportedCases',
+      async (id) => { 
+        console.log(id)
+        const toast = await this.toastController.create({
+          message: "Report Submitted!",
+          duration: 2500
+        })
+        toast.present()
+        this.router.navigate(['/my-reports'])
+      },
+      async (err) => {
+        console.log(err)
+        const toast = await this.toastController.create({
+          message: "An error occured. Please try again later",
+          duration: 2500
+        })
+        toast.present()
       })
-      toast.present()
-      this.router.navigate(['/my-reports'])
-    },
-    async (err) => {
-      console.log(err)
+    } catch (e) {
+      console.log(e)
       const toast = await this.toastController.create({
         message: "An error occured. Please try again later",
         duration: 2500
       })
       toast.present()
-    })
+    }
   }
 
 }
