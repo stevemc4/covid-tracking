097cb80414231152817ed7d4dd5ad620c754e68b
Dhika Rizky <deka_r@outlook.com>
2020-12-05 06:05:46 +0700

    Added delete prompt

--- src/app/pages/my-reports/my-reports.page.ts
+++ src/app/pages/my-reports/my-reports.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore'
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular'
 import { Observable } from 'rxjs';
 
 import RegionHelper from '../../helper/region'
@@ -16,7 +17,7 @@ export class MyReportsPage implements OnInit {
   data: Observable<any[]>
   region: typeof RegionHelper
 
-  constructor(private router: Router, private firestore: AngularFirestore) {
+  constructor(private router: Router, private firestore: AngularFirestore, private alertController: AlertController) {
     this.data = this.firestore.collection('reportedCases').valueChanges({ idField: 'id' })
     this.region = RegionHelper
   }
@@ -26,6 +27,31 @@ export class MyReportsPage implements OnInit {
 
   onFabClick() {
     this.router.navigate(['/new-report'])
+  }
+
+  async handleDelete(caseData: any) {
+    const alert = await this.alertController.create({
+      header: `Delete ${caseData.name}`,
+      message: `Are you sure want to delete ${caseData.name}'s case?`,
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'primary',
+          cssClass: 'danger',
+          handler: () => this.deleteItem(caseData.id)
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        }
+      ]
+    })
+    alert.present()
+  }
+
+  async deleteItem(id: string) {
+
   }
 
 }
